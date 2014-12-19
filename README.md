b72e95ac6e6b1742bdc60d3e6feccec3d97ffcdd
Guillaume Fillon <guillaume@posteo.de>
2014-12-19 20:16:49 +0100

    Update README.md: installation an usage

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-gotech
+Gotech
 ======
 
 A Golang library for Epitech Intra Webservice
+
+Installation
+======
+
+```sh
+go get -u https://github.com/kokaz/gotek
+```
+
+Usage
+======
+
+Here is a little sample to log into the Epitech Intranet, get the dashboard and get your schedule
+
+```go
+client := &gotech.Client{Login: "login_x", Password: "unix_password"}
+dashboard, _ := client.SignIn()
+schedule, _ := client.GetSchedule()
+```
+
+Check out the [sample](https://github.com/kokaz/gotek/blob/master/sample "Sample") folder to get the whole code
---
+++ sample/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+	"github.com/kokaz/gotek"
+)
+
+func readLine(prompt string) (input string, err error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(prompt)
+	input, err = reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return string(input), nil
+}
+
+func readPassword(prompt string) (passwd string, err error) {
+	fmt.Print(prompt)
+
+	password, err := terminal.ReadPassword(syscall.Stdin)
+	if err != nil {
+		return "", err
+	}
+	fmt.Print("\n")
+
+	return string(password), nil
+}
+
+func main() {
+	login, _ := readLine("Login: ")
+	password, _ := readPassword("Password: ")
+	client := &gotek.Client{Login: login, Password: password}
+
+	dashboard, err := client.SignIn()
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	fmt.Println(dashboard.Infos.Id)
+
+	schedule, err := client.GetSchedule()
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	for _, event := range schedule {
+		if len(event.Scolaryear) > 0 {
+			fmt.Printf("%#v\n", event)
+		}
+	}
+}
